fc41be3bdbe79587066cf7e6810c9f08314e4922
mouenen <g1135236859@gmial.com>
2018-08-02 11:36:33 +0900

    initialize devise for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :microposts
-    validates :name, length: { in:6..20,message:'must in 6 - 20 byte'}
+    # validates :name, length: { in:6..20,message:'must in 6 - 20 byte'}
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
