f64994769e08dcc6979f6abff3558994440e6241
mouenen <g1135236859@gmial.com>
2018-08-02 11:56:00 +0900

    customize devise controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :microposts
-    # validates :name, length: { in:6..20,message:'must in 6 - 20 byte'}
+    validates :name, length: { in:6..20,message:'must in 6 - 20 byte'}
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
