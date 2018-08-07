7360c1cbb1d47a5573b7bfa8806abb62e40a11ba
mouenen <g1135236859@gmial.com>
2018-08-07 10:02:17 +0900

    Counter Cache

--- app/models/category_micropost.rb
+++ app/models/category_micropost.rb
@@ -1,4 +1,5 @@
 class CategoryMicropost < ApplicationRecord
     belongs_to :category
     belongs_to :micropost
+    belongs_to :category, counter_cache: :microposts_count
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_084628) do
+ActiveRecord::Schema.define(version: 2018_08_06_110910) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_08_03_084628) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "microposts_count"
   end
 
   create_table "category_microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -49,9 +50,9 @@ ActiveRecord::Schema.define(version: 2018_08_03_084628) do
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "comment"
-    t.integer "micropost_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "micropost_id"
   end
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
